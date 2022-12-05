e27806599c05673d6d60fedf5f51685f3b94e1fd
Łukasz Łaszek <laszek.lukasz@gmail.com>
2022-12-05 21:57:12 +0100

    Add vehicle model class

---
+++ src/main/java/pl/gambeat007/ucl/vehicle/Vehicle.java
@@ -0,0 +1,25 @@
+package pl.gambeat007.ucl.vehicle;
+
+import lombok.*;
+import pl.gambeat007.ucl.model.FuelType;
+import pl.gambeat007.ucl.model.UCLBase;
+
+import javax.persistence.*;
+
+@Data
+
+@Entity
+@Table(name = "vehicles")
+public class Vehicle extends UCLBase {
+    @Column(name = "make")
+    private String make;
+    @Column(name = "model")
+    private String model;
+    @Column(name = "prod_year")
+    private int prodYear;
+    @Column(name = "price")
+    private double price;
+    @Column(name = "fuel_type")
+    @Enumerated(EnumType.STRING)
+    private FuelType fuelType;
+}
