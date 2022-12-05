2fd02e9320fe929d323faf64343022fa207e70ad
Łukasz Łaszek <laszek.lukasz@gmail.com>
2022-12-05 15:58:45 +0100

    Modify main application class

---
+++ src/main/java/pl/gambeat007/ucl/UsedCarsLTDApplication.java
@@ -0,0 +1,15 @@
+package pl.gambeat007.ucl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+public class UsedCarsLTDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsedCarsLTDApplication.class, args);
+    }
+
+}
