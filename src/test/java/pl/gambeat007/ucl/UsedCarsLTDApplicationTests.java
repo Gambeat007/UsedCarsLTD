cc6b67f6ed3cdf0fb55d787adb08ce6d73efe9fe
Łukasz Łaszek <laszek.lukasz@gmail.com>
2022-12-05 16:07:12 +0100

    Add sample test class

---
+++ src/test/java/pl/gambeat007/ucl/UsedCarsLTDApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gambeat007.ucl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedCarsLTDApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
