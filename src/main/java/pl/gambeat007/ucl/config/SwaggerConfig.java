a24b1db0e021ebeaa8ef56407d1ec2c148a5fa03
Łukasz Łaszek <laszek.lukasz@gmail.com>
2022-12-05 15:50:14 +0100

    Add Swagger configuration

---
+++ src/main/java/pl/gambeat007/ucl/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package pl.gambeat007.ucl.config;
+
+import org.springframework.context.annotation.*;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/*
+SwaggerUI url:
+http://localhost:8080/swagger-ui/index.html
+ */
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors. regex("^(?!/(error).*$).*$" ))
+                .build();
+    }
+}
