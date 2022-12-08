db5e5a84b2f43e7340eab6da120a27b1d49c3919
Łukasz Łaszek <laszek.lukasz@gmail.com>
2022-12-08 17:13:27 +0100

    Remove user details interface

--- src/main/java/pl/gambeat007/ucl/security/services/UserDetailsService.java
+++
@@ -1,9 +0,0 @@
-package pl.gambeat007.ucl.security.services;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public interface UserDetailsService {
-
-    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
-}
