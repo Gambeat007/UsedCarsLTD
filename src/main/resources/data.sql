814c86593ad9a6638b69d4092b8173f467925890
Łukasz Łaszek <laszek.lukasz@gmail.com>
2022-12-09 20:35:55 +0100

    Add new table & modify insertions

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,17 @@
-INSERT INTO VEHICLES (id, make, model, prod_year, price, fuel_type)
-VALUES
-(1, 'Skoda', 'Superb', 2018, 99500.50, 'DIESEL'),
-(2, 'Skoda', 'Rapid', 2015, 45200.00, 'PETROL'),
-(3, 'Jaguar', 'XF', 2022, 290000.00, 'DIESEL');
+INSERT INTO VEHICLES(make, model, prod_year, price, fuel_type) VALUES
+('Skoda', 'Superb', 2018, 99500.50, 'DIESEL'),
+('Skoda', 'Rapid', 2015, 45200.00, 'PETROL'),
+('Jaguar', 'XF', 2022, 290000.00, 'DIESEL');
 
-INSERT INTO CUSTOMERS (id, name, email, password, transaction_type, vehicle_id)
-VALUES
-(1, 'Nowak Jan', 'nowak.jan@nowak.com', 'nowak1234', 'SALE', 1),
-(2, 'Nowak Karol', 'nowak.karol@nowak.com', '123456', 'SALE', 2),
-(3, 'Maliniak Jan', 'maliniak@gmail.com', 'haslo1234', 'PURCHASE', 3);
+INSERT INTO CUSTOMERS(name, email, password) VALUES
+('Nowak Jan', 'nowak.jan@nowak.com', 'nowak1234'),
+('Nowak Karol', 'nowak.karol@nowak.com', '123456'),
+('Maliniak Jan', 'maliniak@gmail.com', 'haslo1234');
+
+INSERT INTO ROLES(name) VALUES
+('ROLE_OWNER'),
+('ROLE_CUSTOMER'),
+('ROLE_PUBLIC');
+
+-- INSERT INTO USERS(username, email, password) VALUES
+-- ('John Owner','owner@owner.com','ownerpass');
