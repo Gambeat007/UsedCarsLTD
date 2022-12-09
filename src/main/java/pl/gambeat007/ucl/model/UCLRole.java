2598702c49991b03033ea0fd3ec610612d7d3a5c
Łukasz Łaszek <laszek.lukasz@gmail.com>
2022-12-09 20:31:30 +0100

    Add clearer comments

--- src/main/java/pl/gambeat007/ucl/model/UCLRole.java
+++ src/main/java/pl/gambeat007/ucl/model/UCLRole.java
@@ -1,7 +1,10 @@
 package pl.gambeat007.ucl.model;
 
 public enum UCLRole {
-    OWNER, // company owner access everywhere
-    CUSTOMER, // company customer - access to own transactions
-    PUBLIC // access to vehicles database
+    // company owner -> access everywhere
+    ROLE_OWNER,
+    // company customer -> wider access than public
+    ROLE_CUSTOMER,
+    // most limited access -> few methods
+    ROLE_PUBLIC
 }
