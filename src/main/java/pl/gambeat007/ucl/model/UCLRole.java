3afa94b7e4a0e615b07e38711b89fef965508ee7
Łukasz Łaszek <laszek.lukasz@gmail.com>
2022-12-09 15:07:01 +0100

    Add user roles enum

---
+++ src/main/java/pl/gambeat007/ucl/model/UCLRole.java
@@ -0,0 +1,7 @@
+package pl.gambeat007.ucl.model;
+
+public enum UCLRole {
+    OWNER, // company owner access everywhere
+    CUSTOMER, // company customer - access to own transactions
+    PUBLIC // access to vehicles database
+}
