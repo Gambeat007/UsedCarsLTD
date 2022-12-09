a96239899cabf0fb6af7f4b084f010e18d636484
Łukasz Łaszek <laszek.lukasz@gmail.com>
2022-12-09 20:33:02 +0100

    Code refactoring

--- src/main/java/pl/gambeat007/ucl/payload/request/SignupRequest.java
+++ src/main/java/pl/gambeat007/ucl/payload/request/SignupRequest.java
@@ -9,18 +9,19 @@ import java.util.Set;
 
 @Getter
 @Setter
-
 public class SignupRequest {
     @NotBlank
-    @Size(max = 20)
+    @Size(min = 3, max = 20)
     private String username;
+
     @NotBlank
     @Size(max = 50)
     @Email
     private String email;
-    private Set<String> role;
+
     @NotBlank
     @Size(min = 6, max = 40)
     private String password;
 
+    private Set<String> role;
 }
