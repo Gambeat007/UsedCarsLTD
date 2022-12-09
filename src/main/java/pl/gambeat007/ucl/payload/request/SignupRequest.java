22b36dc31b2f87755e33b68013972042b46141f5
Łukasz Łaszek <laszek.lukasz@gmail.com>
2022-12-09 12:15:53 +0100

    Add signup request for payload

---
+++ src/main/java/pl/gambeat007/ucl/payload/request/SignupRequest.java
@@ -0,0 +1,26 @@
+package pl.gambeat007.ucl.payload.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+@Getter
+@Setter
+
+public class SignupRequest {
+    @NotBlank
+    @Size(max = 20)
+    private String username;
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+    private Set<String> role;
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+}
