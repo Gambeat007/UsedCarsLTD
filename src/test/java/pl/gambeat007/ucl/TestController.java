5010011b0fc54068687b0ea1e0fe799ae01dcd34
Łukasz Łaszek <laszek.lukasz@gmail.com>
2022-12-10 18:03:34 +0100

    Add comments & descriptions, reformat code

--- src/test/java/pl/gambeat007/ucl/TestController.java
+++ src/test/java/pl/gambeat007/ucl/TestController.java
@@ -1,4 +1,4 @@
-package pl.gambeat007.ucl.controllers;
+package pl.gambeat007.ucl;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -6,12 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller test class for users access levels check & maintain.
+ */
+
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/ucl/test")
 public class TestController {
 
+    // Each one of method below checks different level of users access and returns a simple String to confirm.
     @GetMapping("/public")
+    @PreAuthorize("hasRole('PUBLIC')")
     public String shouldAccessPublicContent() {
         return "Public content.";
     }
