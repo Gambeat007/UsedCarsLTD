4a770239ec57263f686c81b342e9d5b13beeee6c
Łukasz Łaszek <laszek.lukasz@gmail.com>
2022-12-10 18:14:14 +0100

    Update lombok annotations & reformat code

--- src/main/java/pl/gambeat007/ucl/user/User.java
+++ src/main/java/pl/gambeat007/ucl/user/User.java
@@ -3,15 +3,24 @@ package pl.gambeat007.ucl.user;
 import lombok.*;
 import pl.gambeat007.ucl.model.UCLBase;
 import pl.gambeat007.ucl.role.Role;
+
 import java.util.HashSet;
 import java.util.Set;
+
 import javax.persistence.*;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+/**
+ * This is a model class for application users
+ */
+
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 
 @Entity
 @Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = "username"), @UniqueConstraint(columnNames = "email")})
@@ -34,12 +43,4 @@ public class User extends UCLBase {
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
-    public User() {
-    }
-
-    public User(String username, String email, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
 }
