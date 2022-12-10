2aed85fbd108737785149c27bc911a6161edbfa3
Łukasz Łaszek <laszek.lukasz@gmail.com>
2022-12-10 17:44:40 +0100

    Update lombok annotations & reformat code

--- src/main/java/pl/gambeat007/ucl/vehicle/Vehicle.java
+++ src/main/java/pl/gambeat007/ucl/vehicle/Vehicle.java
@@ -1,26 +1,31 @@
 package pl.gambeat007.ucl.vehicle;
 
 import lombok.*;
-import pl.gambeat007.ucl.model.FuelType;
 import pl.gambeat007.ucl.model.UCLBase;
 
 import javax.persistence.*;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 
 @Entity
 @Table(name = "vehicles")
 public class Vehicle extends UCLBase {
+
     @Column(name = "make")
     private String make;
+
     @Column(name = "model")
     private String model;
+
     @Column(name = "prod_year")
     private int prodYear;
+
     @Column(name = "price")
     private double price;
+
     @Column(name = "fuel_type")
     @Enumerated(EnumType.STRING)
     private FuelType fuelType;
