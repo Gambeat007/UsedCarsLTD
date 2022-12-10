503dbcda662a706aa54bc3cd07d3734783ef455e
Łukasz Łaszek <laszek.lukasz@gmail.com>
2022-12-10 18:23:08 +0100

    Add comments & descriptions

--- src/main/java/pl/gambeat007/ucl/role/RoleRepository.java
+++ src/main/java/pl/gambeat007/ucl/role/RoleRepository.java
@@ -1,11 +1,14 @@
-package pl.gambeat007.ucl.security.role;
-
+package pl.gambeat007.ucl.role;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pl.gambeat007.ucl.model.UCLRole;
 
 import java.util.Optional;
+
+/**
+ * Repository class with JpaRepository (and it's methods) implemented plus
+ * additional own methods and queries defined
+ */
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
--- src/main/java/pl/gambeat007/ucl/user/UserRepository.java
+++ src/main/java/pl/gambeat007/ucl/user/UserRepository.java
@@ -5,6 +5,11 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * Repository class with JpaRepository (and it's methods) implemented plus
+ * additional own methods and queries defined
+ */
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
