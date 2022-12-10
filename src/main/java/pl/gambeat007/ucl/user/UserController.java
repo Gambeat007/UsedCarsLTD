4d4e77aa73c40e53eee3e870da138edc2461d436
Łukasz Łaszek <laszek.lukasz@gmail.com>
2022-12-10 17:59:39 +0100

    Add comments & descriptions, reformat code

--- src/main/java/pl/gambeat007/ucl/user/UserController.java
+++ src/main/java/pl/gambeat007/ucl/user/UserController.java
@@ -1,22 +1,27 @@
 package pl.gambeat007.ucl.user;
 
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.gambeat007.ucl.vehicle.Vehicle;
 
 import java.util.List;
+
+/**
+ *  Controller cLass for maintaining mapped user method(s),
+ *  available only for company owner
+ */
 
 @RestController
 @RequestMapping("/ucl")
 @RequiredArgsConstructor
 public class UserController {
 
-    UserRepository userRepository;
+    private final UserRepository userRepository;
 
     // retrieve all users with their roles
     @GetMapping("/users")
@@ -25,7 +30,7 @@ public class UserController {
         try {
             return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
