894ef669783ff904dee8c33995373bcd3407b2bf
Łukasz Łaszek <laszek.lukasz@gmail.com>
2022-12-11 09:40:15 +0100

    Add comments & descriptions

--- src/main/java/pl/gambeat007/ucl/user/services/UserDetailsServiceImpl.java
+++ src/main/java/pl/gambeat007/ucl/user/services/UserDetailsServiceImpl.java
@@ -11,6 +11,11 @@ import org.springframework.transaction.annotation.Transactional;
 import pl.gambeat007.ucl.user.User;
 import pl.gambeat007.ucl.user.UserRepository;
 
+/**
+ * Service class implements UserDetailsService interface and override its method,
+ * this is needed to get UserDetails object
+ */
+
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -19,8 +24,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // getting custom User with userRepository
         User user = userRepository.findByUsername(username).orElseThrow(() ->
                 new UsernameNotFoundException("User: " + username + " not found."));
+        // build UserDetails
         return UserDetailsImpl.build(user);
     }
 }
