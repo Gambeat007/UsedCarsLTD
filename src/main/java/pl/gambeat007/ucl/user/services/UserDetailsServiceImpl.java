d7c37c195de8f53378b4fe832d158d9dadad46ff
Łukasz Łaszek <laszek.lukasz@gmail.com>
2022-12-11 09:39:26 +0100

    Update dependency injection

--- src/main/java/pl/gambeat007/ucl/user/services/UserDetailsServiceImpl.java
+++ src/main/java/pl/gambeat007/ucl/user/services/UserDetailsServiceImpl.java
@@ -1,18 +1,20 @@
 package pl.gambeat007.ucl.user.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import pl.gambeat007.ucl.user.User;
 import pl.gambeat007.ucl.user.UserRepository;
 
 @Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
-    @Autowired
-    UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
     @Transactional
